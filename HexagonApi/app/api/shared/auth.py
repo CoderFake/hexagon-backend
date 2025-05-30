fb41751a5ee33f5d5a1b1516ca5a1207d5086833
coderfake <hoangdieu22022002@gmail.com>
2025-05-30 09:27:34 +0700

    update: add user image in storage

--- HexagonApi/app/api/shared/auth.py
+++ HexagonApi/app/api/shared/auth.py
@@ -7,46 +7,36 @@ import app.model.composite as c
 from .errors import abort, abort_with
 
 
-Me = TypeVar('Me')
+User = TypeVar('User')
 
 
 @dataclass
-class Authorized(Generic[Me]):
-    me: Me
+class Authorized(Generic[User]):
+    User: User
     claims: dict[str, Any]
 
 
-class Authorization(Generic[Me]):
-    """
-    Type of dependency object that performs Bearer authentication.
-    """
+class Authorization(Generic[User]):
+
     async def __call__(
         self,
         authorization: Optional[str] = Header(default=None),
-    ) -> Authorized[Me]:
-        """
-        Performs Bearer authentication against the Authorization header and returns an authentication object with the obtained user information.
-
-        Args:
-            authorization: Authorization header.
-        Returns:
-            Authentication object.
-        """
+    ) -> Authorized[User]:
         if not authorization:
             return Authorized(self.no_auth(), {})
         elif not authorization.startswith('Bearer '):
-            abort(401, code=Errors.UNAUTHORIZED.name, message="Invalid authorization header")
+            abort(401, code=Errors.UNAUTHORIZED.name, Userssage="Invalid authorization header")
 
         claims = r.auth.verify(authorization[7:])
 
-        me = await self.authorize(claims)
+        User = await self.authorize(claims)
 
-        return Authorized(me, claims)
+        return Authorized(User, claims)
 
-    def no_auth(self) -> Me:
-        abort(401, code=Errors.UNAUTHORIZED.name, message="Bearer token is not set")
+    def no_auth(self) -> None:
+        abort(401, code=Errors.UNAUTHORIZED.name, Userssage="Bearer token is not set")
 
-    async def authorize(self, claims: dict[str, Any]) -> Me:
+    async def authorize(self, claims: dict[str, Any]) -> User:
         raise NotImplementedError()
 
 
@@ -55,18 +45,18 @@ class WithToken(Authorization[None]):
         return None
 
 
-class WithUser(Authorization[c.Me]):
-    async def authorize(self, claims: dict[str, Any]) -> c.Me:
+class WithUser(Authorization[c.User]):
+    async def authorize(self, claims: dict[str, Any]) -> c.User:
         from app.service.account import login
 
         return (await login(claims['sub'])).or_else(abort_with(401, Errors.NOT_SIGNED_UP.name, "Not signed up yet"))
 
 
-class MaybeUser(Authorization[Optional[c.Me]]):
-    def no_auth(self, *args) -> Optional[c.Me]:
+class MaybeUser(Authorization[Optional[c.User]]):
+    def no_auth(self, *args) -> Optional[c.User]:
         return None
 
-    async def authorize(self, claims: dict[str, Any]) -> Optional[c.Me]:
+    async def authorize(self, claims: dict[str, Any]) -> Optional[c.User]:
         from app.service.account import login
 
         return (await login(claims['sub'])).or_else(self.no_auth)
